41366c7ad6fb8a5393a96ec61b6d6e46e9e07c0c
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-07 19:20:39 +0600

    Добавлен ';' для блоков кода в task1.js

--- task1.js
+++ task1.js
@@ -1,13 +1,13 @@
-const thirdString = document.getElementById('string-3')
-console.log(thirdString)
-const fifthString = document.getElementById('string-5')
-console.log(fifthString)
-const secondString = document.getElementById('string-2')
-console.log(secondString)
-const sixthString = document.getElementById('string-6')
-console.log(sixthString)
-const fourthString = document.getElementById('string-4')
-console.log(fourthString)
-const firstString = document.getElementById('string-1')
-console.log(firstString)
+const thirdString = document.getElementById('string-3');
+console.log(thirdString);
+const fifthString = document.getElementById('string-5');
+console.log(fifthString);
+const secondString = document.getElementById('string-2');
+console.log(secondString);
+const sixthString = document.getElementById('string-6');
+console.log(sixthString);
+const fourthString = document.getElementById('string-4');
+console.log(fourthString);
+const firstString = document.getElementById('string-1');
+console.log(firstString);
 
